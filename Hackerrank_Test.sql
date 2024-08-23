1e9e6c4217bc6449dda06c57aba790670463d799
Thuy Pham <ptbt.thuypham@gmail.com>
2024-08-23 14:48:37 +0800

    Hackerrank_Test.sql

--- Hackerrank_Test.sql
+++ Hackerrank_Test.sql
@@ -1,3 +1,4 @@
+--Merit Rewards
 SELECT
 a.employee_ID,
 a.name
@@ -5,4 +6,15 @@ FROM employee_information AS a
 JOIN last_quarter_bonus AS b
 ON a.employee_ID= b.employee_ID
 WHERE a.division = 'HR'
-AND b.bonus >=5000
+AND b.bonus >=5000,
+
+--Country codes
+SELECT
+a.customer_id,
+a.name,
+'+' || b.country_code ||a.phone_number
+FROM customers AS a
+JOIN country_codes AS b
+ON a.country = b.country
+ORDER BY a.customer_id;
+
